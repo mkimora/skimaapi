fc200dd51a709d788ae5cc9aefa4e4ec35b356d8
kimora <guissendiaye24@gmail.com>
2019-08-05 08:03:59 +0200

    ok

--- src/Controller/OperationController.php
+++ src/Controller/OperationController.php
@@ -4,35 +4,53 @@ namespace App\Controller;
 
 use Symfony\Flex\Unpack\Operation;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Response;
 
-class OperationController extends AbstractController
-{
- /**    
+
+/**    
  * @Route("/api")
  */
-class SecuriteController extends AbstractController
+class OperationController extends AbstractController
 {
-   /**
+    /**
      * @Route("/operation", name="operation")
      */
-   
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+
+    public function register(Request $request, EntityManagerInterface $entityManager)
     {
         $values = json_decode($request->getContent());
         if (isset($values->username, $values->password)) {
             $operation = new Operation();
-            $operation->setSoldeAnterieur($values->soldeAnterieur);
-            $operation->setNouveauSolde($values->nouveauSolde);
-            $operation->setDateDepot($values->dateDepot);
-            $entityManager->persist($operation);
-            $entityManager->flush();   
+
+            if (($values->nouveauSolde) >= 75000) {
+
+
+                $operation->setSoldeAnterieur($values->soldeAnterieur);
+                $operation->setNouveauSolde($values->nouveauSolde);
+                $operation->setDateDepot($values->dateDepot);
+                $entityManager->persist($operation);
+                $entityManager->flush();
+
+
+
+                $data = [
+                    'statut' => 201,
+                    'mess' => 'L\'utilisateur a été créé'
+                ];
+
+                return new JsonResponse($data, 201);
+            } else {
+                $data = [
+                    'statut' => 500,
+                    'mess' => 'l\'opération a échoué'
+                ];
+                return new JsonResponse($data, 500);
+            }
         }
     }
-
-
 }
-
